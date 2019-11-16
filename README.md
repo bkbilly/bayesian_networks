dbf2603a8f7765dea1517e494c2e225a143154dd
bkbilly <bkbilly@hotmail.com>
2019-11-16 21:50:14 +0000

    added setup and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bayesian Networks
+
+Implementation for bayesian network with 
+
+  - Enumeration
+  - Rejection Sampling
+  - Likelihood Weighting
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bayesian-networks',
+    version='0.5',
+    author="bkbilly",
+    author_email="bkbilly@hotmail.com",
+    description="Implementation for bayesian network with Enumeration, Rejection Sampling and Likelihood Weighting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bkbilly/bayesian-networks",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pytz>=2019.2',
+        'matplotlib>=3.1.1',
+        'networkx>=2.4',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
